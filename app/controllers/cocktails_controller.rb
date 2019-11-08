a2d26ad679fcd594dd7a24322ffa397b3b030414
Maria Groves <mariagrovesh@gmail.com>
2019-11-08 18:26:00 +0100

    first v of app

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -37,7 +37,7 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :details, :photo)
   end
 
   def set_cocktail
